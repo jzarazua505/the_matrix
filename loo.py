813ece6c4e5882debb2ba77096165cdd5e496dca
Julian Zarazua <julianzarazua@Julians-MacBook-Pro.local>
2020-09-10 20:06:56 -0500

    modified loo.py and created triangle.py

--- loo.py
+++ loo.py
@@ -6,13 +6,13 @@
 
 tri_size = int(input("How big do you want the triangle? "))
 
-for m in range(1, tri_size + 1):
-    for n in range(1, m + 1):
-        print(n, end=" ")
+for x in range(1, tri_size + 1):
+    for y in range(1, x + 1):
+        print(y, end=" ")
     print()
 
-for m in range(1, 7):
-    for n in range(1, 6):
-        print(f"{m},{n}", end=" ")
-    print()
+# for m in range(1, 7):
+#     for n in range(1, 6):
+#         print(f"{m},{n}", end=" ")
+#     print()
     
---
+++ triangle.py
@@ -0,0 +1,6 @@
+for x in range(5, 0, -1):
+    for y in range(x, 0, -1):
+        print(y, end=" ")
+    print()
+
+print()
