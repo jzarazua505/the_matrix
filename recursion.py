ad2677a286fd10afca76e9c6d3d8cc05372b9362
Julian Zarazua <julianzarazua505@gmail.com>
2021-02-17 20:01:41 -0600

    added power function

--- recursion.py
+++ recursion.py
@@ -4,4 +4,17 @@ def hello(num):
     hello(num - 1)
     print("hello")
 
-hello(0)
+# hello(0)
+
+def power(num, exp):
+    if exp == 0:
+        return 1
+    if num == 0:
+        return 0
+    if num == 1:
+        return 1
+    if exp == 1:
+        return num
+    return num * power(num, exp - 1)
+    
+print(power(9, 11))
