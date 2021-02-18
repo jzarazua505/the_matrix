56af15098a7c514c244cbc52bbc39d11a47947ce
Julian Zarazua <julianzarazua505@gmail.com>
2021-02-17 20:02:36 -0600

    added data_structures.py

---
+++ data_structures.py
@@ -0,0 +1,18 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def push(self, new_item):
+        self.items.append(new_item)
+    
+    def pop(self):
+        items = self.items
+        return items.pop(len(items) - 1)
+
+if __name__ == "__main__":
+    pancake = Stack()
+    pancake.push("butter")
+    pancake.push("syrup")
+    print(pancake.items)
+    print(pancake.pop())
+    print(pancake.items)
