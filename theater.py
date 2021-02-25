514db93a75d61df805218c8124bc97c33e76105d
Julian Zarazua <julianzarazua505@gmail.com>
2021-02-24 20:12:11 -0600

    changed theater.py

--- theater.py
+++ theater.py
@@ -1,6 +1,6 @@
 class MovieWatcher:
     def __init__(self):  
-        self.next = None
+        self.front = None
 
 class MovieColumn:
     def __init__(self):
@@ -11,15 +11,24 @@ class MovieColumn:
         out_string = ""
         while current_seat is not None:
             out_string += "o    "
-            current_seat = current_seat.next
+            current_seat = current_seat.front
         out_string += "\n"
         current_seat = self.head
         while current_seat is not None:
             out_string += "∩ -> "
-            current_seat = current_seat.next
+            current_seat = current_seat.front
         out_string += "SCREEN"
         return out_string
 
     def add_watcher(self, new_watcher):
-        new_watcher.next = self.head
+        new_watcher.front = self.head
         self.head = new_watcher
+
+if __name__ == "__main__":
+    mc = MovieColumn()
+    mc.add_watcher(MovieWatcher())
+    print(mc)
+    mc.add_watcher(MovieWatcher())
+    mc.add_watcher(MovieWatcher())
+    mc.add_watcher(MovieWatcher())
+    print(mc)
