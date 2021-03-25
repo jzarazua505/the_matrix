f3f693cf7fe951ba79620de7d0ee9b46bfcc6e9f
Julian Zarazua <julianzarazua505@gmail.com>
2021-03-24 20:03:59 -0500

    added finished hangman.py

---
+++ hangman.py
@@ -0,0 +1,56 @@
+def show_board(phrase, guesses):
+    for c in phrase:
+        if c in guesses or c == " ":
+            print(c, end=" ")
+        else:
+            print("_", end=" ")
+    print()
+
+def show_mistakes(phrase, guesses):
+    print("Wrong answers:", end=" ")
+    for c in guesses:
+        if c not in phrase:
+            print(c, end=" ")
+    print()
+         
+def get_guess(guesses):
+    valid = False
+    while not valid:
+        # ask player for guess
+        players_guess = input("What letter are you guessing? ")[0:1].lower()
+        if not players_guess.isalpha():
+            print("That is not a letter. Guess again")
+        elif players_guess in guesses:
+            print("You already guessed that letter. Guess again.")
+        else:
+            valid = True
+    return players_guess
+
+def win(phrase, guesses):
+    for c in phrase:
+        if c not in guesses and c != " ":
+            return False
+    return True
+
+if __name__ == "__main__":
+    # setup
+    phrase = "flashing lights"
+    guesses = set()
+    mistakes = 0
+    max_mistakes = 6
+    while mistakes != max_mistakes and not win(phrase, guesses):
+        show_board(phrase, guesses)
+        # shows the players mistakes
+        show_mistakes(phrase, guesses)
+        # show the player the number of wwrong guesses they have left
+        print(f"Chances left: {max_mistakes - mistakes}")
+        # returns the players guess
+        players_guess = get_guess(guesses)
+        # check if guess is wrong
+        if players_guess not in phrase:
+            mistakes += 1
+        guesses.add(players_guess)
+    if mistakes == max_mistakes:
+        print(f"You lose. The phrase was '{phrase}'.")
+    else:
+        print("You win")
