541e7a5d8e013db2d60606870ed6dbe35fbfe652
Julian Zarazua <julianzarazua505@gmail.com>
2021-04-08 19:21:42 -0500

    added insertion_sort func and imported time in sorting.py

--- sorting.py
+++ sorting.py
@@ -1,3 +1,5 @@
+from time import time
+
 def bubble_sort(nums):
     sorted = False
     while not sorted:
@@ -8,6 +10,19 @@ def bubble_sort(nums):
                 sorted = False
     return nums
 
+def insertion_sort(nums):
+    for i in range(1, len(nums)):
+        j = i
+        while j > 0 and nums[j-1] > nums[j]:
+            nums[j-1], nums[j] = (nums[j], nums[j-1])
+            j = j - 1
+    return nums
+
 if __name__ == "__main__":
     list_1 = [1, 7, 9, 4, 3, 4, 2]
+    start_time = time()
     print(bubble_sort(list_1))
+    print((time() - start_time) * 100000)
+    start_time = time()
+    print(insertion_sort(list_1))
+    print((time() - start_time) * 100000)
